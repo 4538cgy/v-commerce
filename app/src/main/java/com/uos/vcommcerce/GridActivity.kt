781776558cec793052e6882c3ff6598f1eda54a0
rroye <rroye@naver.com>
2020-09-21 21:48:11 +0900

    GridRecycler
    GridRecyclerView 레이아웃이랑 테스트기능 세팅

--- app/src/main/java/com/uos/vcommcerce/GridActivity.kt
+++ app/src/main/java/com/uos/vcommcerce/GridActivity.kt
@@ -2,15 +2,43 @@ package com.uos.vcommcerce
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_recycler_grid_view.*
 
 class GridActivity : AppCompatActivity(){
+    val DataList = arrayListOf(
+        GridData(R.drawable.ic_launcher_background, "0번"),
+        GridData(R.drawable.ic_launcher_background, "1번"),
+        GridData(R.drawable.btn_signin_facebook, "2번"),
+        GridData(R.drawable.ic_launcher_background, "3번"),
+        GridData(R.drawable.ic_launcher_background, "4번"),
+        GridData(R.drawable.ic_launcher_background, "5번"),
+        GridData(R.drawable.com_facebook_auth_dialog_cancel_background, "6번"),
+        GridData(R.drawable.ic_launcher_background, "7번"),
+        GridData(R.drawable.ic_launcher_background, "8번"),
+        GridData(R.drawable.com_facebook_button_icon_white, "9번"),
+        GridData(R.drawable.ic_launcher_background, "10번"),
+        GridData(R.drawable.com_facebook_tooltip_black_xout, "11번"),
+        GridData(R.drawable.common_google_signin_btn_text_disabled, "12번"),
+        GridData(R.drawable.ic_launcher_background, "13번"),
+        GridData(R.drawable.ic_launcher_background, "14번"),
+        GridData(R.drawable.messenger_bubble_large_white, "15번"),
+        GridData(R.drawable.ic_launcher_background, "16번"),
+        GridData(R.drawable.messenger_button_white_bg_selector, "17번"),
+        GridData(R.drawable.ic_launcher_background, "18번"),
+        GridData(R.drawable.ic_launcher_background, "19번"),
+        GridData(R.drawable.ic_launcher_background, "20번"),
+        GridData(R.drawable.ic_launcher_background, "21번"),
+        GridData(R.drawable.ic_launcher_background, "22번"),
+        GridData(R.drawable.ic_launcher_background, "23번"),
+        GridData(R.drawable.ic_launcher_background, "24번"))
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_grid_view)
 
-        recyclerGridView.layoutManager = LinearLayoutManager(this)
-        recyclerGridView.adapter = RecyclerGridViewAdapter()
+        //recyclerGridView.layoutManager = LinearLayoutManager(this)
+        recyclerGridView.layoutManager = GridLayoutManager(this,3)
+        recyclerGridView.adapter = RecyclerGridViewAdapter(DataList, this)
     }
 }
--- app/src/main/java/com/uos/vcommcerce/RecyclerGridViewAdapter.kt
+++ app/src/main/java/com/uos/vcommcerce/RecyclerGridViewAdapter.kt
@@ -1,19 +1,44 @@
 package com.uos.vcommcerce
 
+import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item.view.*
+import kotlinx.android.synthetic.main.recycler_grid_item.view.*
 
-class RecyclerGridViewHolder(v : View) : RecyclerView.ViewHolder(v){}
-class RecyclerGridViewAdapter : RecyclerView.Adapter<RecyclerGridViewHolder>() {
+//그리드 데이터
+class GridData(val img:Int, val title:String)
+
+
+class RecyclerGridViewHolder(v : View) : RecyclerView.ViewHolder(v){
+
+    val img = v.content_Img
+    val title = v.content_Text
+}
+
+class RecyclerGridViewAdapter(val DataList:ArrayList<GridData>, val context: Context) : RecyclerView.Adapter<RecyclerGridViewHolder>() {
+    //생성하는부분
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerGridViewHolder {
-        val cellForRow = LayoutInflater.from(parent.context).inflate(R.layout.recycler_grid_item, parent, false)
+        val cellForRow = LayoutInflater.from(context).inflate(R.layout.recycler_grid_item, parent, false)
         return RecyclerGridViewHolder(cellForRow);
     }
+    //보여줄 개수
+    override fun getItemCount() = DataList.size
 
+    //수정하는부분
     override fun onBindViewHolder(holder: RecyclerGridViewHolder, position: Int) {
-    }
+        val data = DataList[position]
+        holder.img.setImageResource(DataList[position].img)
+        holder.title.text = DataList[position].title
 
-    override fun getItemCount() = 15
+        //그리드 안 이미지(item) 클릭시 나중에 영상 재생?
+        holder.itemView.setOnClickListener{
+        Toast.makeText(context, data.title, Toast.LENGTH_SHORT).show()
+        }
+
+    }
 }
