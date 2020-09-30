c8b52e42e12c65c424a0ce04a3f73bb44347b29c
rroye <rroye@naver.com>
2020-09-30 10:14:30 +0900

    innerclass
    innerclass 로 수정

--- app/src/main/java/com/uos/vcommcerce/GridActivity.kt
+++ app/src/main/java/com/uos/vcommcerce/GridActivity.kt
@@ -3,6 +3,7 @@ package com.uos.vcommcerce
 import android.Manifest
 import android.app.Activity
 import android.content.ContentValues
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -11,12 +12,18 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_recycler_grid_view.*
+import kotlinx.android.synthetic.main.recycler_grid_item.view.*
 import java.io.FileOutputStream
 import java.lang.Exception
 import java.text.SimpleDateFormat
@@ -202,6 +209,41 @@ class GridActivity : AppCompatActivity(){
             }
         }
     }
+
+
+    //그리드 데이터
+    inner class GridData(val img:Int, val title:String)
+
+
+    inner class RecyclerGridViewHolder(v : View) : RecyclerView.ViewHolder(v){
+
+        val img = v.content_Img
+        val title = v.content_Text
+    }
+
+    inner class RecyclerGridViewAdapter(val DataList:ArrayList<GridData>, val context: Context) : RecyclerView.Adapter<RecyclerGridViewHolder>() {
+        //생성하는부분
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerGridViewHolder {
+            val cellForRow = LayoutInflater.from(context).inflate(R.layout.recycler_grid_item, parent, false)
+            return RecyclerGridViewHolder(cellForRow);
+        }
+        //보여줄 개수
+        override fun getItemCount() = DataList.size
+
+        //수정하는부분
+        override fun onBindViewHolder(holder: RecyclerGridViewHolder, position: Int) {
+            val data = DataList[position]
+            holder.img.setImageResource(DataList[position].img)
+            holder.title.text = DataList[position].title
+
+            //그리드 안 이미지(item) 클릭시 나중에 영상 재생?
+            holder.itemView.setOnClickListener{
+                Toast.makeText(context, data.title, Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
 }
+
 
 
