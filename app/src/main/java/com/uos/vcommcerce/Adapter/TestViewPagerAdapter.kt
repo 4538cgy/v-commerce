6316fd311dcb02b9569a2f2226c83c1a1f40a442
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-10-06 16:14:05 +0900

    commit video pager in mainActivity
    from hard coding

--- app/src/main/java/com/uos/vcommcerce/Adapter/TestViewPagerAdapter.kt
+++ app/src/main/java/com/uos/vcommcerce/Adapter/TestViewPagerAdapter.kt
@@ -6,20 +6,22 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerControlView
 import com.uos.vcommcerce.Imm
 import com.uos.vcommcerce.R
 import com.uos.vcommcerce.Util.MainBottomSlideUp
 import com.uos.vcommcerce.Util.MainTopSlideDown
 import com.uos.vcommcerce.Util.TopBottomState
-import com.uos.vcommcerce.Util.dp
-import com.uos.vcommcerce.isTopViewOpen
 import com.uos.vcommcerce.topBottomState
+import kotlinx.android.synthetic.main.item_exoplayer.view.*
 import kotlin.math.abs
 
 class TestViewPagerAdapter(private val context: Context, private val items: ArrayList<String>) : RecyclerView.Adapter<TestViewPagerAdapter.ViewHolder>(){
+
+
 
     //이미지뷰 터치 시작위치 측정
     companion object{
@@ -27,30 +29,90 @@ class TestViewPagerAdapter(private val context: Context, private val items: Arra
     }
 
 
-
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-         val imageUrl: ImageView = view.findViewById(R.id.iv_image)
+        //val imageUrl : ImageView = view.findViewById(R.id.iv_image)
+        val exoPlayer : View = view.findViewById(R.id.item_exoplayer)
      }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
-        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false))
-
+        //ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item, parent, false))
+        ViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_exoplayer,
+                parent,
+                false
+            )
+        )
 
     override fun getItemCount(): Int = items.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        Glide.with(context).load(items[position]).into(holder.imageUrl)
+        var view = holder.itemView
+
+        var player = SimpleExoPlayer.Builder(context).build()
+
+        //Glide.with(context).load(items[position]).into(holder.imageUrl)
+        var mediaItem = items[position]
+        view.item_exoplayer.player = player
+        view.item_exoplayer.hideController()
+        view.item_exoplayer.setOnTouchListener{
+                _, motionEvent ->
+
+            when (motionEvent?.action) {
+                //창을 눌럿을떄
+                MotionEvent.ACTION_DOWN -> {
+                    TouchPoint = motionEvent.getY().toInt();
+                }
+                //움직임이 감지 됫을때
+                MotionEvent.ACTION_MOVE -> {
+                }
+
+                //손땟을때
+                MotionEvent.ACTION_UP -> {
+                    var distance: Int = TouchPoint!!.minus(motionEvent.getY().toInt());
+                    if (abs(distance) > 50) {//드래기일시 해당하는 창을 열기
+                        if (distance > 0) {
+                            Log.d("드레그 UP : ", "드레그 UP")
+                            MainBottomSlideUp.instance.SlideUp();
+                        } else {
+                            Log.d("드레그 DOWN : ", "드레그 DOWN")
+                            MainTopSlideDown.instance.SlideDown();
+                        }
+                    } else {//터치일시 각 창을 닫음
+                        returnDefaultView()
+                    }
+                }
+            }
+
+            return@setOnTouchListener false
+        }
+
+
+        player?.setMediaItem(MediaItem.fromUri(mediaItem))
+        player?.prepare()
+        player?.play()
+
+
+
 
         //뷰에 터치리스너 추가
+
         holder.itemView.setOnClickListener(ViewPageClickListner)
         holder.itemView.setOnTouchListener(ViewPageTouchListner)
+
+
     }
 
 
 
+
+
     //최석우 뷰 컨트롤을위한 클릭과 터치리스너
-    val ViewPageClickListner =object : View.OnClickListener{ override fun onClick(v: View?) {} }
-    val ViewPageTouchListner =object : View.OnTouchListener {
+
+    val ViewPageClickListner = object : View.OnClickListener{ override fun onClick(v: View?) {} }
+
+
+    val ViewPageTouchListner = object : View.OnTouchListener {
         override fun onTouch(v: View?, event: MotionEvent?): Boolean {
             when (event?.action) {
                 //창을 눌럿을떄
@@ -58,20 +120,21 @@ class TestViewPagerAdapter(private val context: Context, private val items: Arra
                     TouchPoint = event.getY().toInt();
                 }
                 //움직임이 감지 됫을때
-                MotionEvent.ACTION_MOVE -> { }
+                MotionEvent.ACTION_MOVE -> {
+                }
 
                 //손땟을때
                 MotionEvent.ACTION_UP -> {
-                    var distance : Int = TouchPoint!!.minus( event.getY().toInt());
-                    if (abs(distance)>50){//드래기일시 해당하는 창을 열기
-                        if (distance>0){
-                            Log.d("드레그 UP : ","드레그 UP")
+                    var distance: Int = TouchPoint!!.minus(event.getY().toInt());
+                    if (abs(distance) > 50) {//드래기일시 해당하는 창을 열기
+                        if (distance > 0) {
+                            Log.d("드레그 UP : ", "드레그 UP")
                             MainBottomSlideUp.instance.SlideUp();
-                        }else{
-                            Log.d("드레그 DOWN : ","드레그 DOWN")
+                        } else {
+                            Log.d("드레그 DOWN : ", "드레그 DOWN")
                             MainTopSlideDown.instance.SlideDown();
                         }
-                    }else{//터치일시 각 창을 닫음
+                    } else {//터치일시 각 창을 닫음
                         returnDefaultView()
                     }
                 }
@@ -80,6 +143,7 @@ class TestViewPagerAdapter(private val context: Context, private val items: Arra
             return false
         }
     }
+
 }
 
 fun returnDefaultView(){
