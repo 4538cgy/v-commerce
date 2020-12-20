fbc9d83417563c0868f799607a7d592d166bc683
rroye <rroye@naver.com>
2020-12-20 23:37:07 +0900

    라이브데이터 적용
    라이브데이터 적용

--- app/src/main/java/com/uos/vcommcerce/HistoryFragment.kt
+++ app/src/main/java/com/uos/vcommcerce/HistoryFragment.kt
@@ -2,6 +2,7 @@ package com.uos.vcommcerce
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,10 +10,14 @@ import androidx.databinding.BaseObservable
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ObservableField
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.uos.vcommcerce.Model.UserDataVM
 import com.uos.vcommcerce.databinding.FragmentHistoryBinding
 import com.uos.vcommcerce.databinding.RecyclerHistoryItemBinding
+import kotlinx.android.synthetic.main.fragment_history.*
 
 
 data class HistoryData(
@@ -21,36 +26,38 @@ data class HistoryData(
 )
 
 class HistoryFragment : Fragment() {
-    val DataList = listOf(
-        HistoryData("history1", "1번"),
-        HistoryData("history2-1", "2.1번"),
-        HistoryData("history2-2", "2.2번"),
-        HistoryData("history3", "3번"),
-        HistoryData("history4", "4번"),
-        HistoryData("history5", "5번"),
-        HistoryData("history6", "6번"),
-        HistoryData("history7-1", "7.1번"),
-        HistoryData("history7-2", "7.2번"),
-        HistoryData("history7-3", "7.3번"),
-        HistoryData("history8", "8번"),
-        HistoryData("history9", "9번"),
-        HistoryData("history10", "10번"),
-        HistoryData("history11", "11번"),
-        HistoryData("history12-1", "12.1번"),
-        HistoryData("history12-2", "12.2번"),
-        HistoryData("history13", "13번"),
-        HistoryData("history14", "14번"),
-        HistoryData("history15", "15번"),
-        HistoryData("history16", "16번"),
-        HistoryData("history17-1", "17.1번"),
-        HistoryData("history17-2", "17.2번"),
-        HistoryData("history17-3", "17.3번"),
-        HistoryData("history18", "18번"),
-        HistoryData("history19", "19번"),
-        HistoryData("history20-여까지", "끄읏-")
-    )
+//    val DataList = listOf(
+//        HistoryData("history1", "1번"),
+//        HistoryData("history2-1", "2.1번"),
+//        HistoryData("history2-2", "2.2번"),
+//        HistoryData("history3", "3번"),
+//        HistoryData("history4", "4번"),
+//        HistoryData("history5", "5번"),
+//        HistoryData("history6", "6번"),
+//        HistoryData("history7-1", "7.1번"),
+//        HistoryData("history7-2", "7.2번"),
+//        HistoryData("history7-3", "7.3번"),
+//        HistoryData("history8", "8번"),
+//        HistoryData("history9", "9번"),
+//        HistoryData("history10", "10번"),
+//        HistoryData("history11", "11번"),
+//        HistoryData("history12-1", "12.1번"),
+//        HistoryData("history12-2", "12.2번"),
+//        HistoryData("history13", "13번"),
+//        HistoryData("history14", "14번"),
+//        HistoryData("history15", "15번"),
+//        HistoryData("history16", "16번"),
+//        HistoryData("history17-1", "17.1번"),
+//        HistoryData("history17-2", "17.2번"),
+//        HistoryData("history17-3", "17.3번"),
+//        HistoryData("history18", "18번"),
+//        HistoryData("history19", "19번"),
+//        HistoryData("history20-여까지", "끄읏-")
+//    )
     lateinit var binding:FragmentHistoryBinding
+    lateinit var userDataViewModel: UserDataVM
     //lateinit var historyView: RecyclerView
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -60,8 +67,11 @@ class HistoryFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_history,container, false)
 
         binding.recyclerHistoryView.layoutManager = LinearLayoutManager(requireContext())
-        binding.recyclerHistoryView.adapter = RecyclerHistoryViewAdapter(DataList,requireContext())
-
+        userDataViewModel = ViewModelProvider(this).get(UserDataVM::class.java)
+        userDataViewModel.historyData.observe(this, Observer {
+            binding.recyclerHistoryView.adapter = RecyclerHistoryViewAdapter(it, requireContext())
+        })
+        //binding.recyclerHistoryView.adapter = RecyclerHistoryViewAdapter(DataList,requireContext())
 
         val fragmentView = binding.root
         //val fragmentView = inflater.inflate(R.layout.fragment_history, null)
