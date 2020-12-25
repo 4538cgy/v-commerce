6a98c3417e72b7687efabe8c360902cbc448dbb4
4538cgy <58514224+4538cgy@users.noreply.github.com>
2020-12-25 19:13:29 +0900

    commit make welcome activity

---
+++ app/src/main/java/com/uos/vcommcerce/activity/signup/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.uos.vcommcerce.activity.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uos.vcommcerce.R
+import com.uos.vcommcerce.databinding.ActivityWelcomeBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
