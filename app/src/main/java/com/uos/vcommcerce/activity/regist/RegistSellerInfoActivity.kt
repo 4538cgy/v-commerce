da99d0eb432bf964f9a5449123a2180c9fd23ff6
jeongupark <jeongu@getaim.co>
2021-01-24 21:01:30 +0900

    판매자 등록에 대한 주석 추가

--- app/src/main/java/com/uos/vcommcerce/activity/regist/RegistSellerInfoActivity.kt
+++ app/src/main/java/com/uos/vcommcerce/activity/regist/RegistSellerInfoActivity.kt
@@ -6,6 +6,10 @@ import androidx.databinding.DataBindingUtil
 import com.uos.vcommcerce.R
 import com.uos.vcommcerce.databinding.ActivityRegistSellerInfoBinding
 
+/**
+ *  2021.1.23 작성자 박정우
+ *  판매자 등록 상세 정보 작성 화면
+ */
 class RegistSellerInfoActivity : AppCompatActivity() {
     lateinit var binding: ActivityRegistSellerInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
