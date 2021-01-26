8a6b2cdd75840b6fc171c2e39d2f7f3609c0f7c8
rroye <rroye@naver.com>
2021-01-27 00:23:07 +0900

    일단  settingActivity 이전상태로
    일단  settingActivity 이전상태로

--- app/src/main/java/com/uos/vcommcerce/SettingActivity.kt
+++ app/src/main/java/com/uos/vcommcerce/SettingActivity.kt
@@ -72,7 +72,6 @@ class SettingActivity : AppCompatActivity() {
             settingDTO.add(SettingDTO("로그아웃"))
             settingDTO.add(SettingDTO("비디오 화면 보기"))
             settingDTO.add(SettingDTO("그리드 화면 보기"))
-            settingDTO.add(SettingDTO("주문 화면 보기"))
             settingDTO.add(SettingDTO("RESTFULL TEST"))
             settingDTO.add(SettingDTO("RESTFULL TEST POST"))
             settingDTO.add(SettingDTO("비디오 리스트 보기"))
@@ -125,9 +124,6 @@ class SettingActivity : AppCompatActivity() {
 
                     "그리드 화면 보기" -> {
                         pageChange("Grid")
-                    }
-                    "주문 화면 보기"->{
-                        pageChange("Order")
                     }
 
                     "비디오 리스트 보기" -> {
---
+++ app/src/main/java/com/uos/vcommcerce/activity/oder/OrderWebViewActivity.kt
@@ -0,0 +1,18 @@
+package com.uos.vcommcerce.activity.oder
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.PersistableBundle
+import android.webkit.WebView
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+private var webView: WebView?= null
+private var resultView: EditText? = null
+private var handler = Handler()
+class OrderWebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+
+    }
+
+}
