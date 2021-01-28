20a3190df0e3eaceab169fc3d612867d51b38472
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-01-28 19:15:30 +0900

    LoginWithEmail 기본동작

--- app/src/main/java/com/uos/vcommcerce/activity/login/LoginWithEmail.kt
+++ app/src/main/java/com/uos/vcommcerce/activity/login/LoginWithEmail.kt
@@ -9,17 +9,21 @@ import android.view.KeyEvent
 import android.view.inputmethod.EditorInfo
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
 import com.uos.vcommcerce.R
 import com.uos.vcommcerce.activity.login.popup.PasswordErrorPopUpActivity
+import com.uos.vcommcerce.activity.signup.SignUpActivity
 import com.uos.vcommcerce.databinding.ActivityLoginWithEmailBinding
+
 
 class LoginWithEmail : AppCompatActivity() {
 
-    lateinit var binding : ActivityLoginWithEmailBinding
+    lateinit var binding: ActivityLoginWithEmailBinding
+    val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_login_with_email)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login_with_email)
         binding.activityloginwithemail = this@LoginWithEmail
 
         binding.activityLoginWithEmailEdittextId.addTextChangedListener(EditWatcher())
@@ -36,25 +40,33 @@ class LoginWithEmail : AppCompatActivity() {
 
         //로그인 버튼
         binding.activityLoginWithEmailButtonLogin.setOnClickListener {
-            val intent = Intent(this,PasswordErrorPopUpActivity::class.java)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-            startActivity(intent)
+
+            loginWithEmail(
+                binding.activityLoginWithEmailEdittextId.text.toString(),
+                binding.activityLoginWithEmailEdittextPassword.text.toString()
+            )
+
         }
 
         //비밀번호 찾기
         binding.activityLoginWithEmailTextviewFindPassword.setOnClickListener {
-
+            startActivity(Intent(this,FindPasswordActivity::class.java))
         }
 
         //회원가입
         binding.activityLoginWithEmailTextviewSignup.setOnClickListener {
-
+            startActivity(Intent(this,SignUpActivity::class.java))
         }
-        
+
+        //뒤로가기
+        binding.activityLoginWithEmailImagebuttonClose.setOnClickListener {
+            finish()
+        }
+
 
     }
 
-    inner class EditWatcher : TextWatcher{
+    inner class EditWatcher : TextWatcher {
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
         }
@@ -64,29 +76,69 @@ class LoginWithEmail : AppCompatActivity() {
         }
 
         override fun afterTextChanged(s: Editable?) {
-            if (binding.activityLoginWithEmailEdittextId.text.length > 2 && binding.activityLoginWithEmailEdittextPassword.text.length > 2){
+            if (binding.activityLoginWithEmailEdittextId.text.length > 2 && binding.activityLoginWithEmailEdittextPassword.text.length > 2) {
                 binding.activityLoginWithEmailButtonLogin.setBackgroundResource(R.drawable.button_background_round_black_16dp)
-            }else{
+            } else {
                 binding.activityLoginWithEmailButtonLogin.setBackgroundResource(R.drawable.button_background_round_gray_16dp)
             }
         }
 
     }
 
-    inner class EventListener : TextView.OnEditorActionListener{
+    inner class EventListener : TextView.OnEditorActionListener {
         override fun onEditorAction(v: TextView?, actionId: Int, event: KeyEvent?): Boolean {
             //키보드가 내려간경우
             var handled = false
-            if (actionId == EditorInfo.IME_ACTION_DONE){
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
                 //완료
-            }else if(actionId == EditorInfo.IME_ACTION_NEXT)
-            {
+            } else if (actionId == EditorInfo.IME_ACTION_NEXT) {
                 //키보드 유지
                 handled = true
             }
-            return  handled
+            return handled
 
         }
 
     }
+
+    private fun loginWithEmail(email: String, password: String) {
+        if (email.isNotEmpty() && email != null && password.isNotEmpty() && password != null) {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        //로그인 성공 state - 0
+                        UpdateUI(0)
+                    } else {
+                        //로그인 실패 state - 1
+                        UpdateUI(1)
+                    }
+                }.addOnFailureListener {
+                    UpdateUI(1)
+                }.addOnCanceledListener {
+                    UpdateUI(2)
+                }
+        }
+    }
+
+
+    private fun UpdateUI(state: Int) {
+        when (state) {
+            0 -> {
+                //로그인 성공시
+            }
+            1 -> {
+                //로그인 실패시
+                val intent = Intent(this, PasswordErrorPopUpActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                startActivity(intent)
+            }
+            2 -> {
+                //로그인 취소시
+            }
+
+        }
+
+    }
+
+
 }
