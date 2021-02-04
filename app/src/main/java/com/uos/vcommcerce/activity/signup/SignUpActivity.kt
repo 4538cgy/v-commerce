d43dd9338cc27a00ea48a9f434998590f26fe5e1
jeongupark <jeongu@getaim.co>
2021-02-05 02:11:21 +0900

    회원가입 비밀번호 (영문대소문자 숫자 특수문자 )

--- app/src/main/java/com/uos/vcommcerce/activity/signup/SignUpActivity.kt
+++ app/src/main/java/com/uos/vcommcerce/activity/signup/SignUpActivity.kt
@@ -7,6 +7,7 @@ import androidx.databinding.DataBindingUtil
 import com.uos.vcommcerce.R
 import com.uos.vcommcerce.activity.popup.ErrorPopUpActivity
 import com.uos.vcommcerce.databinding.ActivitySignUpBinding
+import java.util.regex.Pattern
 
 
 class SignUpActivity : AppCompatActivity() {
@@ -29,7 +30,10 @@ class SignUpActivity : AppCompatActivity() {
             with(binding) {
                 // 입력 이메일 체크
                 //TODO 이메일 인증 기능 추가 필요
-                if (activitySignUpEdittextEmail.text.toString().isEmpty() || !checkEmail(activitySignUpEdittextEmail.text.toString())) {
+                if (activitySignUpEdittextEmail.text.toString().isEmpty() || !checkEmail(
+                        activitySignUpEdittextEmail.text.toString()
+                    )
+                ) {
                     val intent = Intent(this@SignUpActivity, ErrorPopUpActivity::class.java)
                     intent.putExtra("title", "잘못 된 이메일")
                     intent.putExtra("content", "이메일 주소를 잘 못 입력 하셨습니다.")
@@ -38,9 +42,7 @@ class SignUpActivity : AppCompatActivity() {
                     return@setOnClickListener
                 }
                 // 비밀 번호 입력 체크
-                //TODO 비밀 번호 형태 체크 필요 (몇자리 수 이상, 그리고 비번 형태 이를 서버에서 체크할지 앱에서 체크할지)
-                if ((activitySignUpEdittextPassword.text.toString() != activitySignUpEdittextPasswordRe.text.toString())
-                    || activitySignUpEdittextPassword.text.isEmpty() || activitySignUpEdittextPasswordRe.text.isEmpty()) {
+                if (!checkPassword(activitySignUpEdittextPassword.text.toString(),activitySignUpEdittextPasswordRe.text.toString())) {
                     val intent = Intent(this@SignUpActivity, ErrorPopUpActivity::class.java)
                     intent.putExtra("title", "확인 비밀번호 불일치")
                     intent.putExtra("content", "비밀번호를 다시 입력해주세요.")
@@ -50,8 +52,8 @@ class SignUpActivity : AppCompatActivity() {
                 }
                 // 모두 입력 되었따면 프로필로 이동
                 val intent = Intent(this@SignUpActivity, ProfileRegistActivity::class.java)
-                intent.putExtra("email",activitySignUpEdittextEmail.text.toString())
-                intent.putExtra("passwor",activitySignUpEdittextPasswordRe.text.toString())
+                intent.putExtra("email", activitySignUpEdittextEmail.text.toString())
+                intent.putExtra("passwor", activitySignUpEdittextPasswordRe.text.toString())
                 intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
                 startActivity(intent)
             }
@@ -59,9 +61,24 @@ class SignUpActivity : AppCompatActivity() {
 
 
     }
+
     // 이메일 형태 체크
-    private fun checkEmail(email: String) : Boolean{
-      return  email.matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex())
+    private fun checkEmail(email: String): Boolean {
+        return email.matches("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+".toRegex())
+    }
+    // 비밀번호 체크 숫자 영문(대소문자) 특수문자 8자 이상
+    private fun checkPassword(password: String, passwordRe: String): Boolean {
+        if (password.isEmpty() || passwordRe.isEmpty()) {
+            return false
+        }
+        if (password != passwordRe) {
+            return false
+        }
+        if(password.length < 8){
+            return  false
+        }
+        val pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~@$!%*#?&])[a-z[A-Z][0-9]$@!%*#?&]{8,}$"
+        return Pattern.matches(pattern,password)
     }
 
 
