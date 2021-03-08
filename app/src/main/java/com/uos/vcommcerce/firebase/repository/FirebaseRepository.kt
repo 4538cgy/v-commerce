51a6b00cf5d68c47d679e573961218ba40e85d21
4538cgy <4538cgy@gmail.com>
2021-03-08 21:05:24 +0900

    commit firebase api - userData

---
+++ app/src/main/java/com/uos/vcommcerce/firebase/repository/FirebaseRepository.kt
@@ -0,0 +1,106 @@
+package com.uos.vcommcerce.firebase.repository
+
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.uos.vcommcerce.datamodel.UserDTO
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.sendBlocking
+import kotlinx.coroutines.flow.callbackFlow
+
+
+class FirebaseRepository {
+
+    /*
+    *
+    *
+
+
+    How to get SnapShot in ViewModel
+    * 0. declare repository
+    * 1. access 'viewModelScope'
+    * 2. launch Dispatchers.IO
+    * 3. access repository
+    * 4. call repository method
+    * 5. collect data
+
+    * ex )
+    viewModelScope.launch(Dispatchers.IO) {
+        repository.Observe().collect {
+            println("data print in viewmodel = " + it.toString())
+        }
+    }
+
+
+    *
+    *
+    */
+
+    private val db = FirebaseFirestore.getInstance()
+
+    //모든 유저의 Field 값 가져오기
+    fun getUserDataAll() = callbackFlow<ArrayList<UserDTO>> {
+        val databaseReference = db.collection("user")
+        val eventListener = databaseReference.addSnapshotListener{ value, error ->
+            var userData : ArrayList<UserDTO> = arrayListOf()
+
+            if(value!!.isEmpty) return@addSnapshotListener
+            value.documents.forEach {
+                userData.add(it.toObject(UserDTO::class.java)!!)
+            }
+            this@callbackFlow.sendBlocking(userData)
+
+        }
+
+        awaitClose { eventListener.remove() }
+    }
+
+    //특정 유저 data 값 한번 가져오기
+    fun getUserData(uid : String) = callbackFlow<UserDTO>{
+
+        val databaseReference = db.collection("user").document(uid)
+        val eventListener = databaseReference.get().addOnCompleteListener {
+            var userData = it.result?.toObject(UserDTO::class.java)
+            if (it.isSuccessful) this@callbackFlow.sendBlocking(userData!!)
+        }.addOnFailureListener {
+            println("----------------------------------------")
+            println("getUserData Task Fail : ${it.toString()}")
+            println("----------------------------------------")
+            this@FirebaseRepository
+        }
+
+    }
+
+    //특정 유저 전체 Data 일괄 Update
+    fun updateUserData(uid : String, data : UserDTO) = callbackFlow<Boolean> {
+        val transactionReference = db.collection("user").document(uid)
+        val runTransaction = db.runTransaction { transaction ->
+            var userData = transaction.get(transactionReference).toObject(UserDTO::class.java)
+
+            transaction.set(transactionReference, userData!!)
+            this@callbackFlow.sendBlocking(true)
+        }.addOnFailureListener {
+            println("--------------------------------------------------")
+            println("updateUserData Transaction Fail : ${it.toString()}")
+            println("--------------------------------------------------")
+            this@callbackFlow.sendBlocking(false)
+        }
+    }
+
+    //특정 유저 데이터 제거
+    fun deleteUserData(uid : String) = callbackFlow<Boolean> {
+        val databaseReference = db.collection("user").document(uid)
+        val eventListener = databaseReference.delete()
+            .addOnSuccessListener { this@callbackFlow.sendBlocking(true) }
+            .addOnFailureListener {
+                println("---------------------------------------------")
+                println("deleteUserData Delete Fail : ${it.toString()}")
+                println("---------------------------------------------")
+            }
+    }
+
+
+
+}
+
+
