03a5f2905a3aefba5d1784191918294158ac57a4
jeongupark <jeongu@getaim.co>
2021-03-11 10:56:03 +0900

    고객 인적 정보 등록 화면 팝업 추가 / Dialog를 이용한 팝업 기능 구현

--- app/src/main/java/com/uos/vcommcerce/activity/signup/ProfileRegistActivity.kt
+++ app/src/main/java/com/uos/vcommcerce/activity/signup/ProfileRegistActivity.kt
@@ -2,9 +2,11 @@ package com.uos.vcommcerce.activity.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.uos.vcommcerce.R
 import com.uos.vcommcerce.databinding.ActivityProfileRegistBinding
+import com.uos.vcommcerce.popup.PopupManager
 
 class ProfileRegistActivity : AppCompatActivity() {
 
@@ -14,6 +16,20 @@ class ProfileRegistActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_profile_regist)
         binding.activityprofileregist = this@ProfileRegistActivity
 
+    }
 
+    fun showPicturePopup(view: View){
+
+        PopupManager(this).pictureOptionPopup(null).show(false)
+
+    }
+    fun setBirthDay(view:View){
+        PopupManager(this).dataPickerPopup(object : PopupManager.DialogClickCallBackStringListener{
+            override fun okBtn(result: String) {
+                if(result.isNotEmpty()){
+                    binding.birthdaytext = result
+                }
+            }
+        }).show(false)
     }
 }
---
+++ app/src/main/java/com/uos/vcommcerce/util/Delegate.kt
@@ -0,0 +1,31 @@
+package com.uos.vcommcerce.util
+
+class Delegate {
+    interface Action {
+        fun run()
+    }
+
+    interface Action1<T1> {
+        fun run(t1: T1)
+    }
+
+    interface Action2<T1, T2> {
+        fun run(t1: T1, t2: T2)
+    }
+
+    interface Action3<T1, T2, T3> {
+        fun run(t1: T1, t2: T2, t3: T3)
+    }
+
+    interface Func<R> {
+        fun call(): R
+    }
+
+    interface Func1<T1, R> {
+        fun call(t: T1): R
+    }
+
+    interface Func2<T1, T2, R> {
+        fun call(t1: T1, t2: T2): R
+    }
+}
