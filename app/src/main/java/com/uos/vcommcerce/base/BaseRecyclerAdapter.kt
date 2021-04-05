076207edfaf5cabd65f77001fea42b0b7ab79afa
Niena <58356541+ChoiSukwoo@users.noreply.github.com>
2021-04-05 23:45:03 +0900

    베이스 리사이클러뷰 어댑터 제작
    데이터 바인딩과 커스텀 리사이클러뷰 어뎁터 이용해서 베이스 리사이클러뷰 어댑터 제작

--- app/src/main/java/com/uos/vcommcerce/base/BaseRecyclerAdapter.kt
+++ app/src/main/java/com/uos/vcommcerce/base/BaseRecyclerAdapter.kt
@@ -1,33 +1,31 @@
 package com.uos.vcommcerce.base
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
-import com.uos.vcommcerce.databinding.ActivityReviewUploadItemBinding
-import com.uos.vcommcerce.databinding.ActivityReviewUploadItemFooterBinding
-import com.uos.vcommcerce.datamodel.ReviewUploadItemDTO
-import com.uos.vcommcerce.datamodel.ReviewUploadItemFooterDTO
-import java.util.*
+import com.uos.vcommcerce.BR
+import com.uos.vcommcerce.databinding.ItemReviewItemBinding
 import kotlin.collections.ArrayList
 
 
-open class BaseRecyclerAdapter<item,viewBinding : ViewDataBinding>( var header: Boolean = false,var footer : Boolean = false ) : RecyclerView.Adapter<BaseRecyclerAdapter<item,viewBinding>.CustomViewHolder>() {
+open class BaseRecyclerAdapter<item,viewBinding : ViewDataBinding>(@LayoutRes val layoutId: Int,itemlist : ArrayList<item>) : RecyclerView.Adapter<BaseRecyclerAdapter<item,viewBinding>.CustomViewHolder>() {
 
-    private val TYPE_HEADER = 0
-    private val TYPE_ITEM = 1
-    private val TYPE_FOOTER = 2
+    //각뷰에 결합될 variable name 은 item 이여야함
 
-    var itemList : ArrayList<item> = arrayListOf();
+    var itemList : ArrayList<item> = arrayListOf()
+
+    init {
+        itemList = itemlist;
+    }
+
 
     //베이스가 될 커스텀 홀더
-    open inner class CustomViewHolder(val binding: viewBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(data: item){
-
+    inner class CustomViewHolder(val binding: viewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun onBind(item : item){
+            binding.setVariable(BR.item,item)
         }
     }
 
@@ -38,54 +36,18 @@ open class BaseRecyclerAdapter<item,viewBinding : ViewDataBinding>( var header: 
 
     //여기서 바인딩을 할당
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        if(viewType == TYPE_FOOTER){
-            val inflaterView = LayoutInflater.from(parent.context)
-            val binding = viewBinding.inflate(inflaterView,parent,false);
-            return ReviewUploadItemFooter(binding)
-        }else{
-            val inflaterView = LayoutInflater.from(parent.context)
-            val binding = ActivityReviewUploadItemBinding.inflate(inflaterView,parent,false);
-            return ReviewUploadItem(binding)
-        }
+        val binding : viewBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),layoutId,parent,false)
+        return CustomViewHolder(binding)
+
     }
 
-    //홀더의 뷰타입을 커스텀으로 설정
-    override fun getItemViewType(position: Int): Int {
-        if(header == true && position == 0){
-            return TYPE_HEADER
-        }
-        if(header == true && footer == true &&position == itemList.count()+1) {
-            return TYPE_FOOTER
-        }
-        if(header == false && footer == true &&position == itemList.count()) {
-            return TYPE_FOOTER
-        }
-        return TYPE_ITEM;
-    }
 
-    //footer를 포함하여 +1개 를 반환
     override fun getItemCount(): Int {
-        var itemCount : Int = itemList.count();
-        if(header == true){
-            itemCount += 1
-        }
-        if(footer == true){
-            itemCount += 1
-        }
-        return itemCount
+        return itemList.count()
     }
 
     //뷰홀더에 데이터 할당
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        when(holder.itemViewType){
-            TYPE_ITEM ->{
-                var holder = holder as ReviewUploadItem
-                holder.onBind(reviewList[position])
-            }
-            TYPE_FOOTER ->{
-                var holder = holder as ReviewUploadItemFooter
-                holder.onBind(ReviewUploadItemFooterDTO(this@ReviewUpload))
-            }
-        }
+        holder.onBind(itemList[position])
     }
 }
